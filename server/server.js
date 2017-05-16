6a135ddc3387f9c38fa88414ba9bcab16586d465
Stuart Bradshaw <stuartb@Hyperion.local>
2017-05-16 22:01:03 +0100

    FEAT: Add GET /todos route

--- server/server.js
+++ server/server.js
@@ -9,6 +9,18 @@ var app = express();
 
 app.use(bodyParser.json());
 
+app.get('/todos', (req, res) => {
+  Todo.find()
+    .then((todos) => {
+      res.send({
+        todos
+      });
+    }, (err) => {
+      res.status(400);
+      res.send(err);
+    });
+});
+
 app.post('/todos', (req, res) => {
   var todo = new Todo({
     text: req.body.text
