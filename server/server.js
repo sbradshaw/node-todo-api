ee671f7f45f28717cb0167109f9cff20fec6d3a3
Stuart Bradshaw <stuartb@Hyperion.local>
2017-05-18 00:46:50 +0100

    FEAT: Add GET /todos/:id

--- server/server.js
+++ server/server.js
@@ -1,5 +1,6 @@
-var express = require('express');
-var bodyParser = require('body-parser');
+const {ObjectID} = require('mongodb');
+const express = require('express');
+const bodyParser = require('body-parser');
 
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
@@ -18,6 +19,30 @@ app.get('/todos', (req, res) => {
     }, (err) => {
       res.status(400);
       res.send(err);
+    });
+});
+
+app.get('/todos/:id', (req, res) => {
+  var id = req.params.id;
+  
+  if (!ObjectID.isValid(id)) {
+    res.status(404).send();
+  }
+
+  Todo.findById(id)
+    .then((todo) => {
+      
+      if (!todo) {
+        return res.status(404).send();
+      } 
+      
+      res.status(200);
+      res.send({
+        todo
+      });
+    })
+    .catch((err) => {
+      res.status(400).send();
     });
 });
 
