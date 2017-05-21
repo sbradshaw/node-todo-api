140b270735bb95de0d8beb2b9deddf8f2a0c3a0e
Stuart Bradshaw <stuartb@Hyperion.local>
2017-05-21 19:36:54 +0100

    TEST: Add tests for PATCH /todos/:id

--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -10,7 +10,9 @@ const todos = [{
   text: 'First test todo'
 }, {
   _id: new ObjectID(),
-  text: 'Second test todo'
+  text: 'Second test todo',
+  completed: true,
+  completedAt: 333
 }];
 
 beforeEach((done) => {
@@ -70,7 +72,8 @@ describe('POST /todos', () => {
           .then((todos) => {
             expect(todos.length).toBe(2);
             done();
-          }).catch((err) => {
+          })
+          .catch((err) => {
             done(err);
           });
       });
@@ -157,6 +160,49 @@ describe('DELETE /todos/:id', () => {
     request(app)
       .delete('/todos/123')
       .expect(404)
+      .end(done);
+  });
+});
+
+describe('PATH /todos/:id', () => {
+  it('should update the todo', (done) => {
+    var text = 'Test todo text';
+    var completed = true;
+    var hexId = todos[0]._id.toHexString();
+
+    request(app)
+      .patch(`/todos/${hexId}`)
+      .send({
+        text: text,
+        completed: completed
+      })
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(text);
+        expect(res.body.todo.completed).toBe(true);
+        expect(res.body.todo.completedAt).toBeA('number');
+      })
+      .end(done);
+  });
+
+  it('should clear completedAt when todo is not completed', (done) => {
+    var text = 'Test todo text';
+    var completed = false;
+    var hexId = todos[1]._id.toHexString();
+
+    request(app)
+      .patch(`/todos/${hexId}`)
+      .send({
+        text: text,
+        completed: completed
+      })
+      .expect(200)
+      .expect((res) => {
+        expect(res.body.todo.text).toBe(text);
+        expect(res.body.todo.completed).toBe(false);
+        expect(res.body.todo.completedAt).toBe(null);
+        expect(res.body.todo.completedAt).toNotExist();
+      })
       .end(done);
   });
 });
