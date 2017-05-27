75e7a622f0f1f9af3c5cf8f8dfa80e6702a4617e
Stuart Bradshaw <stuartb@Hyperion.local>
2017-05-27 17:41:11 +0100

    FEAT: Add GET /users/me route and authentication middleware

--- server/server.js
+++ server/server.js
@@ -8,6 +8,7 @@ const {ObjectID} = require('mongodb');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 var port = process.env.PORT;
 
@@ -143,6 +144,10 @@ app.post('/users', (req, res) => {
       res.status(400);
       res.send(err);
     });
+});
+
+app.get('/users/me', authenticate, (req, res) => {
+  res.send(req.user);  
 });
 
 app.listen(port, () => {
