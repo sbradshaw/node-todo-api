cf3b0721973bb1c0fea95781c8180304224fdc85
Stuart Bradshaw <stuartb@Hyperion.local>
2017-05-28 16:41:26 +0100

    TEST: Add test for GET /users/me and POST /users

--- server/server.js
+++ server/server.js
@@ -1,3 +1,5 @@
+// server.js
+
 require('./config/config.js');
 
 const _ = require('lodash');
@@ -5,10 +7,10 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const {ObjectID} = require('mongodb');
 
-var {mongoose} = require('./db/mongoose');
-var {Todo} = require('./models/todo');
-var {User} = require('./models/user');
-var {authenticate} = require('./middleware/authenticate');
+const {mongoose} = require('./db/mongoose');
+const {Todo} = require('./models/todo');
+const {User} = require('./models/user');
+const {authenticate} = require('./middleware/authenticate');
 
 var port = process.env.PORT;
 
@@ -146,8 +148,9 @@ app.post('/users', (req, res) => {
     });
 });
 
+// GET /users/me
 app.get('/users/me', authenticate, (req, res) => {
-  res.send(req.user);  
+  res.send(req.user);
 });
 
 app.listen(port, () => {
--- server/tests/server.test.js
+++ server/tests/server.test.js
@@ -4,26 +4,11 @@ const {ObjectID} = require('mongodb');
 
 const {app} = require('./../server');
 const {Todo} = require('./../models/todo');
+const {User} = require('./../models/user');
+const {todos, populateTodos, users, populateUsers} = require('./seed/seed');
 
-const todos = [{
-  _id: new ObjectID(),
-  text: 'First test todo'
-}, {
-  _id: new ObjectID(),
-  text: 'Second test todo',
-  completed: true,
-  completedAt: 333
-}];
-
-beforeEach((done) => {
-  Todo.remove({})
-    .then(() => {
-      return Todo.insertMany(todos);
-    })
-    .then(() => {
-      done();
-    });
-});
+beforeEach(populateUsers);
+beforeEach(populateTodos);
 
 describe('POST /todos', () => {
   it('should create a new todo', (done) => {
@@ -204,6 +189,91 @@ describe('PATH /todos/:id', () => {
         expect(res.body.todo.completedAt).toBe(null);
         expect(res.body.todo.completedAt).toNotExist();
       })
+      .end(done);
+  });
+});
+
+describe('GET /users/me', () => {
+  it('should return user if authenticated', (done) => {
+    request(app)
+      .get('/users/me')
+      .set('x-auth', users[0].tokens[0].token)
+      .expect(200)
+      .expect((res) => {
+        expect(res.body._id).toBe(users[0]._id.toHexString());
+        expect(res.body.email).toBe(users[0].email);
+      })
+      .end(done);
+  });
+
+  it('should return 401 if not authenticated', (done) => {
+    request(app)
+      .get('/users/me')
+      .expect(401)
+      .expect((res) => {
+        expect(res.body).toEqual({});
+      })
+      .end(done);
+  });
+});
+
+describe('POST /users', () => {
+  it('should create a user', (done) => {
+    var email = 'example@example.com';
+    var password = '123abcd';
+
+    request(app)
+      .post('/users')
+      .send({
+        email: email,
+        password: password
+      })
+      .expect(200)
+      .expect((res) => {
+        expect(res.headers['x-auth']).toExist();
+        expect(res.body._id).toExist();
+        expect(res.body.email).toBe('example@example.com');
+      })
+      .end((err) => {
+
+        if (err) {
+          return done();
+        }
+
+        User.findOne({email})
+          .then((user) => {
+            expect(user).toExist();
+            expect(user.password).toNotBe(password);
+            done();
+          });
+      });
+  });
+
+  it('should return validation errors if request is invalid', (done) => {
+    var email = 'example';
+    var password = '123';
+    
+    request(app)
+      .post('/users')
+      .send({
+        email: email,
+        password: password
+      })
+      .expect(400)
+      .end(done);
+  });
+
+  it('should not create a user if an email is in use', (done) => {
+    var email = 'andrew@example.com';
+    var password = '123abcde';
+    
+    request(app)
+      .post('/users')
+      .send({
+        email: email,
+        password: password
+      })
+      .expect(400)
       .end(done);
   });
 });
